345c9a387aa3b1845fb688b6e3f505cffbc08391
skyMattos <victor-gusto2012@hotmail.com>
2022-07-19 23:57:47 -0300

    First commit: Creating backend

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import  express  from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({limit:'30mb',extended:true}))
+app.use(bodyParser.urlencoded({limit:'30mb',extended:true}))
+app.use(cors());
+
+const CONNECTION_URL= 'mongodb+srv://SkyWalker:Deathtail2@cluster-memory-app.2m6sapf.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT,() => console.log(`Server running on port: ${PORT}`)))
+    .catch((err) => console.log(err.message));
+
+
+//https://www.mongodb.com/cloud/atlas
